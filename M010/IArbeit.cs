ac1086547c3c26272ede0e0333a362ee34f920de
ppedvLukasK <lukask@ppedv.de>
2023-04-21 09:39:47 +0200

    M010

---
+++ M010/IArbeit.cs
@@ -0,0 +1,28 @@
+﻿namespace M010;
+
+public interface IArbeit //Interfaces fangen per Konvention mit I an
+{
+	static readonly int Wochenstunden = 40; //statisches Feld über IArbeit.Wochenstunden angreifen
+
+	int Gehalt { get; set; } //Properties werden weitergegeben
+
+	string Job { get; set; }
+
+	void Lohnauszahlung(); //Methoden ohne Body
+
+	int Jahresgehalt();
+
+	int LohnProStunde(int lohn);
+
+	public void Test()
+	{
+		//Bad Practice
+	}
+}
+
+public interface ITeilzeitArbeit
+{
+	static readonly int Wochenstunden = 20;
+
+	void Lohnauszahlung();
+}
---
+++ M010/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace M010;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+
+		Mensch m = new Mensch();
+		m.Gehalt = 5000;
+		m.Job = "Softwareentwickler";
+		//m.Lohnauszahlung(); //Lohnauszahlungsmethode durch Interface
+
+		IArbeit a = m; //Variable vom Typ IArbeit genau wie bei Vererbung
+		a.Lohnauszahlung();
+
+		ITeilzeitArbeit ta = m;
+		ta.Lohnauszahlung();
+
+        Console.WriteLine($"Der Mitarbeiter verdient {m.Jahresgehalt()}€ pro Jahr.");
+		Console.WriteLine($"Der Mitarbeiter verdient {m.LohnProStunde(m.Gehalt / 4)}€ pro Stunde.");
+
+		//IEnumerable: Basisinterface von allen Listen in C#
+		IEnumerable<int> e1 = new int[10];
+		IEnumerable<int> e2 = new List<int>();
+		IEnumerable<int> e3 = new Stack<int>();
+
+		Test(e1); //Methode akzeptiert alle Listen
+		Test(e2);
+		Test(e3);
+
+		if (m is IArbeit) //Überprüfen ob ein Interface an einem Objekt angehängt ist
+		{
+
+		}
+    }
+
+	static void Test<T>(IEnumerable<T> l)
+	{
+
+	}
+}
+
+public class Lebewesen { }
+
+public class Mensch : Lebewesen, IArbeit, ITeilzeitArbeit
+{
+	public int Gehalt { get; set; }
+
+	public string Job { get; set; }
+
+	public int LohnProStunde(int lohn)
+	{
+		return lohn / IArbeit.Wochenstunden;
+	}
+
+	public int Jahresgehalt()
+	{
+		return Gehalt * 12;
+	}
+
+	void IArbeit.Lohnauszahlung()
+	{
+        Console.WriteLine($"Dieser Mitarbeiter hat ein Gehalt von {Gehalt}€ für den Job {Job} bekommen. " +
+			$"Er arbeitet {IArbeit.Wochenstunden} Stunden pro Woche.");
+    }
+
+	void ITeilzeitArbeit.Lohnauszahlung()
+	{
+		Console.WriteLine($"Dieser Mitarbeiter hat ein Gehalt von {Gehalt / 2}€ für den Job {Job} bekommen. " +
+			$"Er arbeitet {ITeilzeitArbeit.Wochenstunden} Stunden pro Woche.");
+	}
+}
